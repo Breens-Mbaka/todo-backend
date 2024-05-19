c969848edd403fe8a81c459b4d3ff530ea6cba05
breensmbaka <breens.mbaka@dlight.com>
2024-05-19 22:03:24 +0300

    Initializing project

---
+++ src/main/kotlin/com/breens/Application.kt
@@ -0,0 +1,13 @@
+package com.breens
+
+import com.breens.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/breens/plugins/Routing.kt
@@ -0,0 +1,10 @@
+package com.breens.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
